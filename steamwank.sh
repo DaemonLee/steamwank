71de6504fb94d46dd081f2b908af5d0aeac85a61
DaemonLee <DaemonLeeSchmidt@gmail.com>
2015-02-18 00:37:28 -0800

    indentation, support for not weird people, exit status

--- steamwank.sh
+++ steamwank.sh
@@ -19,33 +19,39 @@ do
 opts="$1"
 
 case $opts in
-    -b|--browser)
-    BROWSER="$2"
-    shift
-    ;;
-    -i|--id)
-    ID="$2"
-    shift
-    ;;
-    -t|--time)
-    TIME="$2"
-    shift
-    ;;
-    *)
-
-    ;;
+  -b|--browser)
+  BROWSER="$2"
+  shift
+  ;;
+  -i|--id)
+  ID="$2"
+  shift
+  ;;
+  -t|--time)
+  TIME="$2"
+  KILL=true
+  shift
+  ;;
+  -k|--kill)
+  KILL=true
+  shift
+  ;;
+  *)
+  ;;
 esac
 shift
 done
 
 if [ -z ${TIME+x} ]; then TIME=2; fi # Maybe to quick for slow computers
 if [ -z ${BROWSER+x} ]; then BROWSER="xdg-open"; fi
-if [ -z ${ID+x} ]; then >&2 echo "Error: Steam group ID needed!"; exit; fi
+if [ -z ${ID+x} ]; then >&2 echo "Error: Steam group ID needed!"; exit 2; fi
 
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH-}:/usr/lib32" # steam-native hotfix
 
 $BROWSER steam://friends/joinchat/$ID &
 
-sleep $TIME
+if $KILL; then
+  sleep $TIME
 
-killall $BROWSER
+  killall $BROWSER
+fi
